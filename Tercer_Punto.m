688cd1c5df36e3b50e2a2e1053104c7d1b69c69c
Juan Diego Plaza Gómez <31490004+jplazag@users.noreply.github.com>
2022-05-13 10:27:24 -0500

    Actualización tercer punto

--- Tercer_Punto.m
+++ Tercer_Punto.m
@@ -1,13 +1,18 @@
 
 close all;
-l = [14.5, 10.7, 10.7, 9]; % Longitudes eslabones
+l = [39 105.7, 106, 65.7]; % Longitudes eslabones
 % Definicion del robot RTB
-L(1) = Link('revolute','alpha',pi/2,'a',0,   'd',l(1),'offset',0,   'qlim',[-3*pi/4 3*pi/4]);
-L(2) = Link('revolute','alpha',0,   'a',l(2),'d',0,   'offset',pi/2,'qlim',[-3*pi/4 3*pi/4]);
+L(1) = Link('revolute','alpha',-pi/2,'a',0,   'd',l(1),'offset',-pi/2,   'qlim',[-3*pi/4 3*pi/4]);
+L(2) = Link('revolute','alpha',0,   'a',l(2),'d',0,   'offset',-pi/2,'qlim',[-3*pi/4 3*pi/4]);
 L(3) = Link('revolute','alpha',0,   'a',l(3),'d',0,   'offset',0,   'qlim',[-3*pi/4 3*pi/4]);
-L(4) = Link('revolute','alpha',0,   'a',0,   'd',0,   'offset',0,   'qlim',[-3*pi/4 3*pi/4]);
+L(4) = Link('revolute','alpha',0,   'a',l(4),   'd',0,   'offset',0,   'qlim',[-3*pi/4 3*pi/4]);
 Robot = SerialLink(L,'name','Px');
-ws = [-50 50];
+
+Robot.tool = [0 0 1 0;
+              1 0 0 0;
+              0 1 0 0;
+              0 0 0 1];
+ws = [-200 200];
 % Graficar robot
 
 %%
@@ -26,12 +31,41 @@ for i=1:4
     call(cliente,msg);
     pause(1);
 end
-Robot.plot(q,'notiles','noname');
-axis([repmat(ws,1,2) 0 60])
+Robot.plot(q*pi/180,'notiles','noname');
 Robot.teach()
 
 %%
+q1 = [0 0 0 0 0];
+q2 = [-20 20 -20 20 0];
+q3 = [30 -30 30 -30 0];
+q4 = [-90 15 -55 17 0];
+q5 = [-90 45 -55 45 10];
+
+Q = [q1; q2; q3; q4; q5];
+
+%%
+msg.AddrName = "Goal_Position";
+pause(2);
+
+for i=1:size(Q,1)
+    for j=1:4
+        msg.Id = j;
+        msg.Value = mapfun(Q(i,j),-150,150,0,1023);
+        call(cliente,msg);
+        pause(1);
+    end
+    
+    Robot.plot(Q(i,1:4)*pi/180,'notiles','noname');
+    Robot.teach()
+    pause(4);
+end
+
+
+
+%%
 rosshutdown
+%%
+mapfun(30,-150,150,0,1023)
 %%
 function output = mapfun(value,fromLow,fromHigh,toLow,toHigh)
 narginchk(5,5)
